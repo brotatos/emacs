9ca101f8c98c94377a5d22e5b5e8038cac46fe73
Robin Choudhury <xrobinc@gmail.com>
2018-12-17 00:45:30 -0800

    Remove duplicate melpa-stable repo.
    Gitignore packages.

--- init.el
+++ init.el
@@ -3,7 +3,6 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
