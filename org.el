c47828fcb55826dd1f9ffdac9b27b6dea7a3e8a5
Robin Choudhury <xrobinc@gmail.com>
2020-06-21 19:35:29 -0700

    org: Properly hook org-bullets-mode.

--- org.el
+++ org.el
@@ -1,6 +1,5 @@
 (use-package org-bullets
   :ensure t
-  :diminish t
-  :hook (org-mode-hook . (lambda () (org-bullets-mode 1))))
+  :hook (org-mode-hook org-bullets-mode))
 
 (setq org-log-done 'time)
