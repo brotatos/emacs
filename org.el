720904f034c8c9e518a3d0f20bba02b2d74a5fe7
Robin Choudhury <xrobinc@gmail.com>
2020-06-26 21:39:40 -0700

    org: Configure publishing.

--- org.el
+++ org.el
@@ -4,3 +4,24 @@
   (add-hook 'org-mode-hook 'org-bullets-mode))
 
 (setq org-log-done 'time)
+;; https://orgmode.org/manual/HTML-preamble-and-postamble.html
+;; disable author + date + validate link at end of HTML exports
+(setq org-html-postamble nil)
+(setq org-publish-use-timestamps-flag nil)
+
+(setq org-publish-project-alist
+      '(("noir"
+         :base-directory "~/noir/"
+         :publishing-directory "~/Dropbox/apps/noir"
+         :section-numbers nil
+         :table-of-contents nil
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         )
+        ("noir-static"
+         :base-directory "~/noir/static/"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory "~/Dropbox/apps/noir/static/"
+         :recursive t
+         :publishing-function org-publish-attachment
+         )))
