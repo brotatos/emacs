82e3fc8ffdb12bc1aaef31400c29bdb2f443d289
Robin Choudhury <xrobinc@gmail.com>
2020-07-19 08:39:51 -0700

    Set org settings in when eval.

--- org.el
+++ org.el
@@ -3,29 +3,10 @@
   :config
   (add-hook 'org-mode-hook 'org-bullets-mode))
 
-(setq org-log-done 'time)
-;; https://orgmode.org/manual/HTML-preamble-and-postamble.html
-;; disable author + date + validate link at end of HTML exports
-(setq org-html-postamble nil)
 
-(setq org-publish-project-alist
-      '(("noir"
-         :base-directory "~/noir/"
-         :publishing-directory "~/Dropbox/apps/noir"
-         :section-numbers nil
-         :table-of-contents nil
-         :recursive t
-         :publishing-function org-html-publish-to-html
-         )
-        ("noir-static"
-         :base-directory "~/noir/static/"
-         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory "~/Dropbox/apps/noir/static/"
-         :recursive t
-         :publishing-function org-publish-attachment
-         )))
 
 (with-eval-after-load 'org
+  (setq org-agenda-files (directory-files-recursively "~/noir" ".*\.org"))
 
   (setq auto-indent-start-org-indent t
         auto-indent-fix-org-return t
@@ -39,6 +20,29 @@
   ;; Where new notes will be stored
   (setq org-mobile-inbox-for-pull "~/noir/flagged.org")
   (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+
+  (setq org-log-done 'time)
+  ;; https://orgmode.org/manual/HTML-preamble-and-postamble.html
+  ;; disable author + date + validate link at end of HTML exports
+  (setq org-html-postamble nil)
+
+  (setq org-publish-project-alist
+        '(("noir"
+           :base-directory "~/noir/"
+           :publishing-directory "~/Dropbox/apps/noir"
+           :section-numbers nil
+           :table-of-contents nil
+           :recursive t
+           :publishing-function org-twbs-publish-to-html
+           )
+          ("noir-static"
+           :base-directory "~/noir/static/"
+           :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+           :publishing-directory "~/Dropbox/apps/noir/static/"
+           :recursive t
+           :publishing-function org-publish-attachment
+           )))
+
   ;; https://blog.aaronbieber.com/2016/03/05/playing-tag-in-org-mode.html#comment-4976528210
   (defun air--org-swap-tags (tags)
     "Replace any tags on the current headline with TAGS.
